aab4493bac199eee1ac00d30c4f569554edc79d8
Burak <burakaltintas01@gmail.com>
2021-04-24 15:56:13 -0500

    PRACTICING COMMIT AND PUSH

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -6,5 +6,7 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("adding Github Now");
         System.out.println("YAAAY STUFFS ARE ON GITHUB");
+        // ADDING FINAL WRAP UP CODE
+
     }
 }
